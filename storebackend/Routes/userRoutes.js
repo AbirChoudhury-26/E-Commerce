b332f55bbe4db8c354af0f0e1a0a34a183c1af91
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-08-23 22:36:32 +0530

    New route added

--- storebackend/Routes/userRoutes.js
+++ storebackend/Routes/userRoutes.js
@@ -3,7 +3,7 @@ import User from "../models/userModels.js";
 import expressAsyncHandler from "express-async-handler";
 import { generateToken } from "../utils.js";
 import bcrypt from 'bcryptjs';
-
+// import User from '../models/userModels.js'
 
 const userRouter= express.Router();
 //  Here we will be searching the User based on its Email being Provided during Signup.
@@ -29,6 +29,29 @@ userRouter.post(
        res.status(401).send({message:"Password doesn't match with Email-Id"});
     }
     res.status(401).send({message:"Email-Id Doesn't Exist"})
+   })
+);
+
+userRouter.post(
+   '/signup',expressAsyncHandler(async(req,res)=>{
+
+//  Here we use a new User Model to save the User Details in our MongoDB with help of Mongoose Connection.
+      const newUser=new User({
+         name:req.body.name,
+         email:req.body.email,
+         password:bcrypt.hashSync(req.body.password),
+         // Converting The plain Text Password into Hashing value to Stire in Encrypted form in Database
+
+      })
+
+      const user=await newUser.save();
+      res.send({
+         _id:user._id,
+         name:user.name,
+         email:user.email,
+         isAdmin:user.isAdmin,
+         token:generateToken(user),
+      })
    })
 )
 
