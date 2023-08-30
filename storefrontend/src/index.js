617b604c9d00ccb2bb2c15d69dc27b782aeeb75e
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-08-30 22:38:35 +0530

    New

--- storefrontend/src/index.js
+++ storefrontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {HelmetProvider} from 'react-helmet-async';
+import {PayPalScriptProvider} from '@paypal/react-paypal-js'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <StoreProvider>
     <HelmetProvider>
+    <PayPalScriptProvider deferLoading={true}>
     <App />
+    </PayPalScriptProvider>
     </HelmetProvider>
     </StoreProvider>
   </React.StrictMode>
