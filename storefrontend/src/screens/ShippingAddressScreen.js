87ff8943ea7cc9d388c867502a95432a980da57b
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-09-01 21:28:49 +0530

    New

--- storefrontend/src/screens/ShippingAddressScreen.js
+++ storefrontend/src/screens/ShippingAddressScreen.js
@@ -1,23 +1,34 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 import { Store } from '../Store';
+import CheckoutSteps from '../Components/CheckoutSteps';
 
 const ShippingAddressScreen = () => {
   const navigate= useNavigate();
 
   const{state,dispatch:ctxDispatch}= useContext(Store);
+  
 
   const{
+    userInfo,
     cart:{shippingAddress},
   }=state;
 
   const [fullName, setFullName] = useState(shippingAddress.fullName|| '');
   const [address, setAddress] = useState(shippingAddress.address||'');
   const [city, setCity] = useState(shippingAddress.city||'');
-  const [postal, setPostal] = useState(shippingAddress.postal||'');
+  const [postalCode, setPostal] = useState(shippingAddress.postalCode||'');
+
+ useEffect(()=>{
+   if(!userInfo)
+   {
+    navigate('/signin?redirect=/shipping')
+   }
+ },[userInfo,navigate]);
+
   const [country, setCountry] = useState(shippingAddress.country||'');
 
   const submitHandler = (e) => {
@@ -29,7 +40,7 @@ const ShippingAddressScreen = () => {
         fullName,
         address,
         city,
-        postal,
+        postalCode,
         country,
       },
     })
@@ -38,7 +49,7 @@ const ShippingAddressScreen = () => {
         fullName,
         address,
         city,
-        postal,
+        postalCode,
         country,
       })
     )
@@ -51,6 +62,9 @@ const ShippingAddressScreen = () => {
       <Helmet>
         <title> Shipping Address</title>
       </Helmet>
+      {/* Here Checkout step1 & step2 are variable */}
+      <CheckoutSteps step1 step2/>
+      
       <div className="container small-container">
         <h1 className="my-3">Shipping Address</h1>
 
@@ -91,17 +105,15 @@ const ShippingAddressScreen = () => {
               onChange={(e) => setCity(e.target.value)}
               required
             />
+           </Form.Group>
 
           <Form.Group className="mb-3" controlId="postal">
             <Form.Label> Postal Code</Form.Label>
             <Form.Control
-              value={postal}
+              value={postalCode}
               onChange={(e) => setPostal(e.target.value)}
               required
             />
-          </Form.Group>
-
-         
           </Form.Group>
 
           <div className="mb-3">
