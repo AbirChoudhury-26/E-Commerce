801716aab0f817a757b9b5577943f2eff9c09a32
AbirChoudhury-26 <abirudaipur@gmail.com>
2023-09-02 23:18:02 +0530

    New

--- storefrontend/src/Components/LoadingBox.js
+++ storefrontend/src/Components/LoadingBox.js
@@ -2,8 +2,11 @@ import Spinner from  'react-bootstrap/Spinner'
  export default function LoadingBox()
 {
     return( 
+    //  console.log('Ruk Jao')
+
     <Spinner animation ="border" role="status">
-       <span className="visually-hidden">Ruk jao...</span>
+       <span className="visually-hidden">
+       </span>
     </Spinner>
     );
 }
